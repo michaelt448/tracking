dfe1f153d53492f9bc77f816921be2d658223a82
michaelt77 <michael.tilman@bluemesahealth.com>
2021-01-12 19:50:11 -0800

    added authenticated requests

--- client/tracking/src/api/tracker.js
+++ client/tracking/src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
-  baseURL: 'http://0c83c47f47ad.ngrok.io'
+const instance = axios.create({
+  baseURL: 'http://0ad1b904554f.ngrok.io'
 })
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('accessToken')
+    console.log('this is the token', token)
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+export default instance
